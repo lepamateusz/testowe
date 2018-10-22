dd3f9611e895c94e5801fa51b9299960f4d3e854
matkapolka <matkapolka@gmail.com>
2018-10-22 18:33:07 +0200

    Pierwszy commit

---
+++ tweepy_streamer.py
@@ -0,0 +1,107 @@
+from tweepy import API
+from tweepy import Cursor
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+import twitter_credentials
+import numpy as np
+import pandas as pd
+
+class TwitterClient():
+    def __init__(self, twitter_user=None):
+        self.auth = TwitterAuthenticator().authenticate_twitter_app()
+        self.twitter_client = API(self.auth)
+        self.twitter_user = twitter_user
+
+    def get_twitter_client_api(self):
+        return self.twitter_client
+
+    def get_user_timeline_tweets(self, num_tweets):
+        tweets = []
+        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
+            tweets.append(tweet)
+        return tweets
+
+    def get_friend_list(self, num_friends):
+        friend_list = []
+        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
+            friend_list.append(friend)
+        return friend_list
+
+    def get_home_timeline_tweets(self, num_tweets):
+        home_timeline_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
+            home_timeline_tweets.append(tweet)
+        return home_timeline_tweets
+
+
+class TwitterAuthenticator():
+
+    def authenticate_twitter_app(self):
+
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY,twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN,twitter_credentials.ACCESS_TOKEN_SECRET)
+        return auth
+
+class TwitterStreamer():
+
+    def __init__(self):
+        self.twitter_autenticator = TwitterAuthenticator()
+
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+            listener =  TwitterListener(fetched_tweets_filename)
+            auth = self.twitter_autenticator.authenticate_twitter_app()
+            stream = Stream(auth, listener)
+            stream.filter(track=hash_tag_list)
+
+class TwitterListener(StreamListener):
+
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
+
+    def on_data(self, data):
+        try:
+            print(data)
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print("Error on_data %s" %str(e))
+        return True
+
+    def on_error(self, status):
+        if status == 420:
+            return False
+        print(status)
+
+class TweetAnalyzer():
+
+    def tweets_to_data_frame(self, tweets):
+        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+        df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['source'] = np.array([tweet.source for tweet in tweets])
+        df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+
+        return df
+
+
+if __name__ == '__main__':
+
+    twitter_client = TwitterClient()
+    tweet_analyzer = TweetAnalyzer()
+    api = twitter_client.get_twitter_client_api()
+
+    # Authenticate using config.py and connect to Twitter Streaming API.
+    # hash_tag_list = ["donal trump", "hillary clinton", "barack obama", "bernie sanders"]
+    # fetched_tweets_filename = "tweets.txt"
+    tweets = api.user_timeline(screen_name="realDonaldTrump", count=20)
+    twitter_client = TwitterClient('pycon')
+    #print(twitter_client.get_friend_list(20))
+
+    df = tweet_analyzer.tweets_to_data_frame(tweets)
+    print(df)
+
+    print(dir(tweets[0]))
